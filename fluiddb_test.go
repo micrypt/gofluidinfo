cabc85cfb85a24d60fd09962b26e1c0ff60ef115
micrypt <micrypt@micrypt-eeepc.(none)>
2010-06-02 19:22:03 +0100

    README fix

--- fluiddb_test.go
+++ fluiddb_test.go
@@ -11,26 +11,24 @@ import (
 
 func main() {
 
-// Call to url pattern "/users/username"
-url := "/users/test"
+    // Call to url pattern "/users/username"
+    url := "/users/test"
 
-myclient := fluiddb.NewClient("test","test")
+    myclient := fluiddb.NewClient("test","test")
 
-r, err := myclient.Get(url)
+    r, err := myclient.Get(url)
 
-var b []byte
-if err == nil {
-    b, err = ioutil.ReadAll(r.Body)
-    r.Body.Close()
-}
+    var b []byte
+    if err == nil {
+        b, err = ioutil.ReadAll(r.Body)
+        r.Body.Close()
+    }
 
-if err != nil {
-    log.Stderr(err)
-} else {
-    fmt.Println(string(b))
-}
-
-
+    if err != nil {
+        log.Stderr(err)
+    } else {
+        fmt.Println(string(b))
+    }
 
 }
 
